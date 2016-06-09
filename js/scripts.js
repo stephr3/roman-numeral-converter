6c58d5451453c088798caa102d60004f73c978c5
Julia Dickey and Stephanie Gurung <julia@>
2016-06-09 11:48:28 -0700

    lunch break

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,21 @@ var roman = ["I", "V", "X", "L", "C", "D", "M"];
 var romanIndex = 0;
 var result = [];
 
-// var converter = function(num){};
-// console.log(userInputArray);
 
+var converter = function(num){
+// console.log(userInputArray);
+};
+
+var iterator = function(num){
+  var userInputArray = userInput.split("").reverse().map(Number);
+  for ( i=0 ; i < userInputArray.length ; i++){
+    
+};
+
+
+
+
+}
 
 
 
@@ -20,7 +32,8 @@ $(function(){
     event.preventDefault();
 
     var userInput = $("#userInput").val();
-    var userInputArray = userInput.split("").reverse().map(Number);
 
+    var romanNumeral = iterator(userInput);
+    $("#result").text(romanNumeral);
   });
 });
